de42aed4915e541f892c14ba277964d6c6e0038e
Matt Beyer <matthewebeyer@hotmail.com>
2020-08-22 13:09:43 +0100

    add npm cron

--- discovery-service/src/data-access.ts
+++ discovery-service/src/data-access.ts
@@ -2,8 +2,8 @@ import * as mongoose from "mongoose";
 import * as  Bluebird from 'bluebird';
 (<any>mongoose).Promise = Bluebird;
 
-const connectionString = `mongodb://ubio_mongoservice_1:27017/ubio`
+const connectionString = `mongodb://${ process.env.MONGO_HOST || 'ubio_mongoservice_1' }:27017/ubio`
 
 console.log(`Collecting to: ${connectionString}`);
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true } );
-export const ubioConnection = mongoose.connection; 
+export const ubioConnection = mongoose.connection;
--- discovery-service/src/utils/remove-expired-groups.ts
+++ discovery-service/src/utils/remove-expired-groups.ts
@@ -1,8 +1,24 @@
+import * as fs from "fs"
+import { CronJob } from 'cron'
+
 import { GroupService } from "../services/group-service";
+import { ubioConnection } from "../../src/data-access";
 
 (async () => {
-    const groupService = new GroupService();
-    const expiryAge = process.env.MILLISECOND_EXPIRY_AGE || '10000';
-    const deleteCount = await groupService.removeExpiredInstances(+expiryAge)
-    console.log(`Remove expired groups count: ${deleteCount}`)
+
+    ubioConnection.on('open', async function () {
+        const groupService = new GroupService();
+        const job = new CronJob('* * * * * *', async function () {
+            const expiryAge = process.env.MILLISECOND_EXPIRY_AGE || '10000';
+            const deleteCount = await groupService.removeExpiredInstances(+expiryAge)
+            let log = fs.readFileSync('./logs/remove-expired-groups').toString();
+            log = log + '\n' + new Date().toString() + `Remove expired groups count: ${deleteCount}`;
+            fs.writeFileSync('./logs/remove-expired-groups', log)
+        });
+        
+        job.start();
+
+
+    });
+
 })();
