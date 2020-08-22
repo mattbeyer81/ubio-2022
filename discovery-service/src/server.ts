a81e547272f6f9bd0a1e60a877da816ff2771f5e
Matt Beyer <matthewebeyer@hotmail.com>
2020-08-22 13:13:46 +0100

    add index.ts

--- discovery-service/src/server.ts
+++ discovery-service/src/server.ts
@@ -1,12 +1,10 @@
 import { app } from "./app";
 
-class Server {
+export default class Server {
     constructor() {
         const port = 3000
         app.listen(port, () => {
-            console.log(`Example app listening at http://localhost:${port}`)
+            console.log(`Listening at http://localhost:${port}`)
         })
     }
 }
-
-export const server = new Server
