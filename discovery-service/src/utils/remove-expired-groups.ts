585bb852cc2dbadb992c95ceb91e7cc3fde41f2b
Matt Beyer <matthewebeyer@hotmail.com>
2020-08-23 21:29:30 +0100

    add example .gitlab-ci.yml

--- discovery-service/src/utils/remove-expired-groups.ts
+++ discovery-service/src/utils/remove-expired-groups.ts
@@ -6,7 +6,7 @@ import { ubioConnection } from "../../src/data-access";
 (async () => {
 
     ubioConnection.on('open', async function () {
-        const job = new CronJob('* * * * * *', async function () {
+        const job = new CronJob('* * * * *', async function () {
             const expiryAge = process.env.MILLISECOND_EXPIRY_AGE || '10000';
             const deleteCount = await applicationService.removeExpiredInstances(+expiryAge)
             console.log('Expired instances - deleted count' + deleteCount);
