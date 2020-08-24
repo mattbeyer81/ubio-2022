55c63c191da5b88cd5263108674a9bf9727a46c3
Matt Beyer <matthewebeyer@hotmail.com>
2020-08-24 08:50:07 +0100

    default expiry -> 1 day

--- discovery-service/src/utils/remove-expired-groups.ts
+++ discovery-service/src/utils/remove-expired-groups.ts
@@ -7,7 +7,7 @@ import { ubioConnection } from "../../src/data-access";
 
     ubioConnection.on('open', async function () {
         const job = new CronJob('* * * * *', async function () {
-            const expiryAge = process.env.MILLISECOND_EXPIRY_AGE || '10000';
+            const expiryAge = process.env.MILLISECOND_EXPIRY_AGE || '86400000';
             const deleteCount = await applicationService.removeExpiredInstances(+expiryAge)
             console.log('Expired instances - deleted count: ' + deleteCount);
         });
